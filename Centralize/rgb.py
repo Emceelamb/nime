733c4e7ffb8f59ea2a3e3683e25947230813bcac
emceelamb <mlam@berkeley.edu>
2019-10-02 01:42:00 -0400

    add led class

--- Centralize/rgb.py
+++ Centralize/rgb.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 from gpiozero import RGBLED
-from signal import pause
 from time import sleep
-import RPi.GPIO as GPIO
 
+class light(object):
+    d
 led = RGBLED(red=22, green=9, blue=10)
 
 led.color = (0,1,1)
 
-for i in range(10):
-    led.red = i/10
-    sleep(0.1)
+def dim:
+    color = 'red'
+    def __init__(self, color):
+        self.color = color
+        if self.color == 'red'
+            color = 'red'
+        if self.color == 'green'
+            color = 'green'
+        if self.color == 'blue'
+
+    def fade:
+        for i in range(10):
+            led.color = i/10
+            sleep(0.1)
 
 
