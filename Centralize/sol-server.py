0e584b202890bcf38e7ece22af942909c84ce00d
emceelamb <mlam@berkeley.edu>
2019-10-01 22:57:33 -0400

    adding notes and solenoid server

--- Centralize/sol-server.py
+++ Centralize/sol-server.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
 import socket
 import sys
+from TriggerSolenoid import *
+
+sol = Solenoid(4)
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,18 +14,20 @@ print(local_hostname)
 ip_address = socket.gethostbyname(local_hostname)
 print(ip_address)
 #server_address = ('192.168.8.145', 10000)
-server_address = ('192.168.8.228', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+server_address = ('', 10000)
+print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 while True:
-    print >>sys.stderr, '\nwaiting to receive message'
+    print('\nwaiting to receive message')
     data, address = sock.recvfrom(4096)
 
-    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >>sys.stderr, data
+    print('received %s bytes from %s' % (len(data), address))
+    print(data)
 
     if data:
-        print "data recvd"
+        print("data recvd")
         sent = sock.sendto(data, address)
-        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+        print('sent %s bytes back to %s' % (sent, address))
+        print("solenoid triggered")
+        sol.trigger()
