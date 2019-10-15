a1fdc8791c4cf0e91e69330dced94eccd9b0dcb1
emceelamb <mlam@berkeley.edu>
2019-10-15 17:57:32 -0400

    improve test-script

--- Centralize/test-script.py
+++ Centralize/test-script.py
@@ -4,12 +4,14 @@ import time
 import random
 
 while True:
-#    random_Number = random.randint(1,3)
-    for node in range(1,3):
+    random_Number = random.randint(1,4)
+    for node in range(1,4):
         if random_Number == 1:
             send(IP(dst="192.168.8.228")/UDP(dport=10000)/Raw(load="Hi!"))
         if random_Number == 2:
             send(IP(dst="192.168.8.170")/UDP(dport=10000)/Raw(load="Hi!"))
         if random_Number == 3:
            send(IP(dst="192.168.8.169")/UDP(dport=10000)/Raw(load="Hi!"))
-        time.sleep(0.5)
+        if random_Number == 4:
+           send(IP(dst="192.168.8.228")/UDP(dport=10000)/Raw(load="servo;\n"))
+        time.sleep(random.random()/2)
