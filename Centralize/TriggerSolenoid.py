b28440f4620138d9cf9a277e761482c66c7555e3
emceelamb <mlam@berkeley.edu>
2019-10-15 17:57:54 -0400

    adjust sleep

--- Centralize/TriggerSolenoid.py
+++ Centralize/TriggerSolenoid.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 from time import sleep
+import time
 
 class Solenoid(object):
     def __init__(self, pin):
@@ -10,7 +11,10 @@ class Solenoid(object):
         print("Solenoid is set to " + str(self.pin))
 
     def trigger(self):
+#        timeout = int(round(time.time() * 1000))+100
         GPIO.output(self.pin, 1)
-        sleep(0.05)
+#        while (int(round(time.time() * 1000))) < timeout:
+#            getTime = int(round(time.time()))
+        time.sleep(0.01)
         GPIO.output(self.pin, 0)
         
